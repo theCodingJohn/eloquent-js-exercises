0277b2214b26d58f701232f23720b0cf13aafaea
John Carlo Cunanan <codingjohn@outlook.com>
2021-02-03 18:26:11 +0800

    refactor aVectorType.js

--- Chapter 6/aVectorType.js
+++ Chapter 6/aVectorType.js
@@ -14,18 +14,16 @@ class Vec {
   }
 
   plus(vec) {
-    this.x += vec.x;
-    this.y += vec.y;
-    return this;
+    return new Vec(this.x + vec.x, this.y + vec.y);
   }
 
   minus(vec) {
-    this.x -= vec.x;
-    this.y -= vec.y;
-    return this;
+    return new Vec(this.x - vec.x, this.y - vec.y);
   }
 
   get length() {
     return Math.sqrt(Math.pow(this.x - 0, 2) + Math.pow(this.y - 0, 2));
   }
 }
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
